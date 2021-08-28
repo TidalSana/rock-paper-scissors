45c570b502aa158713f8570ff096b5f9598ff4da
Joshua Semana <jsemana054@gmail.com>
2021-08-28 14:15:36 -0600

    UI addition to the game

--- script.js
+++ script.js
@@ -1,75 +1,86 @@
+let playerSelection = "";
 let choices = ["rock", "paper", "scissors"];
 let playerScore = 0;
 let computerScore = 0;
 
-//Function that runs the game 5 times
-let game = function() {
-    for (let plays = 0; plays < 5; plays++) {
-    // Variable that runs the random array picker for computer
-    let randomAnswer = choices[Math.floor(Math.random()*choices.length)];
+const buttons = document.querySelectorAll('button');
+const playerP = document.getElementById('player');
+const computerP = document.getElementById('computer'); 
+const roundPlay = document.getElementById('roundPlay')
 
-    // Variable to store the random array answer to a parameter for computer
-    let computerSelection = randomAnswer; 
-    let playerSelection = prompt("rock paper or scissors?").toLowerCase();
-
-    // Here we run the function 
-    playRound(playerSelection, computerSelection)
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    if(button.id === "1") {
+      playerSelection = "rock";
+    } else if (button.id === "2") {
+      playerSelection = "paper";
+    } else {
+      playerSelection = "scissors";
     }
-    declareWinner(playerScore, computerScore);
-};
+    game();
+  });
+});
 
-// Function that calculates who wins 
-// Also adds the points for who wins.
+let game = function() {
+    let randomAnswer = choices[Math.floor(Math.random()*choices.length)];
+    let computerSelection = randomAnswer;
+    
+    playRound(playerSelection, computerSelection)
+    playerP.textContent = "Player: " + playerScore;
+    computerP.textContent = "Computer: " + computerScore;
+   
+    if (playerScore > computerScore && playerScore == 5) {
+        alert("Player has " + playerScore + " points.");
+        alert("Computer has " + computerScore + " points.")
+        alert("Player has more points! Player wins!");
+        playerScore = 0;
+        computerScore = 0;
+    } else if (playerScore < computerScore && computerScore == 5) {
+        alert("Computer has "+ computerScore + " points.");
+        alert("Player has " + playerScore + " points.")
+        alert("Computer has more points! Computer Wins!");
+        playerScore = 0;
+        computerScore = 0;
+    } else if (playerScore == 5 && computerScore == 5) {
+        alert("Both equal! We have a tie!");   
+        playerScore = 0;
+        computerScore = 0;
+    } 
+}
+
 let playRound = function(a, b) {
     // Here if the player calls rock we call different situations based on rock
     if (a === "rock") {
-    if (b === "rock") {
-        alert("Both rock? Tie.");
-    } else if (b === "paper") {
-        alert("Rock vs Paper? Paper Wins!");
-        computerScore++;
-    } else {
-        alert("Rock vs Scissors? Rock Win!");
-        playerScore++;
-    }
-    
+        if (b === "rock") {
+            roundPlay.textContent = "Both rock? Tie.";
+        } else if (b === "paper") {
+            roundPlay.textContent = "Rock vs Paper? Paper Wins!";
+            computerScore++;
+        } else {
+            roundPlay.textContent = "Rock vs Scissors? Rock Win!";
+            playerScore++;
+        }
     // Here if the player calls paper we call different situations based on paper
     } else if (a === "paper") {
-    if (b === "rock") {
-        alert("Paper vs rock? Paper Wins!");
-        playerScore++;
-    } else if (b === "paper") {
-        alert("Paper vs Paper? Tie.");
-    } else {
-        alert("Paper vs Scissors? Scissors Win!");
-        computerScore++;
-    }
-
+        if (b === "rock") {
+            roundPlay.textContent = "Paper vs rock? Paper Wins!";
+            playerScore++;
+        } else if (b === "paper") {
+            roundPlay.textContent = "Paper vs Paper? Tie.";
+        } else {
+            roundPlay.textContent = "Paper vs Scissors? Scissors Win!";
+            computerScore++;
+        }
     // Here if the player calls paper we call different situations based on paper
     } else {
-    if (b === "scissors") {
-        alert("Scissors vs Scissors? Tie.");
-    } else if (b === "paper") {
-        alert("Scissors vs Paper? Scissors Win!");
-        playerScore++
-    } else {
-        alert("Scissors vs Rock? Rock Win!");
-        computerScore++;
-    }
-    }
-};
-
-// Function considers who has more points and declares winner
-let declareWinner = function(playerScore, computerScore) {
-    if (playerScore > computerScore) {
-    alert("Player has " + playerScore + " points.");
-    alert("Player has more points! Player wins!");
-    } else if (playerScore < computerScore) {
-    alert("Computer has "+ computerScore + " points.");
-    alert("Computer has more points! Computer Wins!");
-    } else {
-    alert("Both equal! We have a tie!");
+        if (b === "scissors") {
+            roundPlay.textContent = "Scissors vs Scissors? Tie.";
+        } else if (b === "paper") {
+            roundPlay.textContent = "Scissors vs Paper? Scissors Win!";
+            playerScore++
+        } else {
+            roundPlay.textContent = "Scissors vs Rock? Rock Win!";
+            computerScore++;
+        }
     }
 };
-
-game();
